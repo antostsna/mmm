8078690de05913ee22c8cefd001018c20f01b0ba
anto112 <07158031@o365.mcut.edu.tw>
2023-07-12 17:14:10 +0800

    update repository name

---
+++ test.py
@@ -0,0 +1,15 @@
+"""This is a very simple function in python script"""
+
+
+def print_func(msg):
+    """
+    This is a simple function that prints a message
+
+    Parameters:
+       msg (str): The string to print
+    """
+
+    print(msg)
+
+
+print_func('Hello World')
